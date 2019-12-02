48edf5285685a35014d1f5cad9ba72cd0309ada7
agojoalessandro <agojo.alessandro@iisgalvanimi.edu.it>
2019-12-02 10:39:24 +0000

    03.2.1

--- COUNTRY_JS/routes/index.js
+++ COUNTRY_JS/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var country = require('express');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/pages/:state/', function(req, res, next){
+ res.render('state', {state: country.info(req.params.state)}) ; //Rendiamo lo stato un parametro
+})
+
 
 module.exports = router;
